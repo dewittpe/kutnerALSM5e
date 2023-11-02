9df799a3bb3d91ec838bff83618e7196532eb083
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-11-02 14:13:53 -0600

    wip

--- R/datasets.R
+++ R/datasets.R
@@ -1,10 +1,19 @@
 #' SENIC
 #'
-#' Study on the Efficany of Nosocomial Infection Control.
+#' Study on the Efficany of Nosocomial Infection Control
 #'
 #' See vignette("kutnerALSM5e-Appendix-C-Data-Sets") for details on this data
 #' set's construciton.
 #'
-#' @format a data.frame
+#' @format a data.frame 
+#' \tabular{clll}{
+#'    \tab column name \tab Variable Name         \tab Description \cr
+#'    \tab ----------- \tab -------------         \tab ----------- \cr
+#'  1 \tab id          \tab Identification Number \tab 1 - 113 \cr
+#'  2 \tab los         \tab Length of Stay        \tab Average Length of Stay of all \cr
+#'    \tab             \tab                       \tab patients in hospital (in days) \cr
+#'  3 \tab age         \tab Age                   \tab Average age of patients (in years)
+#'  4 \tab             \tab                       \tab \cr
+#' }
 #'
 "SENIC"
--- tests/test-datasets.R
+++ tests/test-datasets.R
@@ -4,3 +4,14 @@
 datasets <- data(package = "kutnerALSM5e")$results
 stopifnot(identical(unname(datasets[, "Item"]), c("SENIC")))
 stopifnot(identical(unname(datasets[, "Title"]), c("SENIC")))
+
+library(kutnerALSM5e)
+
+################################################################################
+# Testing for SENIC data
+stopifnot(identical(nrow(SENIC), 113L))
+stopifnot(identical(ncol(SENIC), 12L))
+
+################################################################################
+##                                End of File                                 ##
+################################################################################
