6a79f0b0f69c68dab7845d0e7dcedd3aba6a6f14
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-11-02 10:47:27 -0600

    add package documention file

---
+++ R/kutnerALSM5e-package.R
@@ -0,0 +1,8 @@
+#' A reproduction of the text book "Applied Linear Statistical Models 5th
+#' Edition" by Kutner, Nachtsheim, Neter, and Li.
+#'
+#' Each chapter of the book is reproduced as a vignette with R code injected for
+#' producing graphics, models, etc.
+#'
+#' @keywords internal
+"_PACKAGE"
